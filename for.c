12347d10eb7d21fb8b834aa443659e337cd75179
jeenklynn <46195953+jeenklynn@users.noreply.github.com>
2022-04-24 14:16:30 +0300

    Update for.c

--- for.c
+++ for.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
-main()
+main(){
 // 10'a kadar olan tüm sayıları 5'er kez yazdıran program.
-{
 	for(int i = 1; i < 10; i++){
 		for(int a = 0; a < 5; a++){
 			printf("%d \n",i);
