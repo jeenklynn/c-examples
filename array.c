371aa344813c784c31610376706b1a13f573cb0c
jeenklynn <46195953+jeenklynn@users.noreply.github.com>
2022-04-24 14:09:04 +0300

    Create array.c

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+main()
+{  
+// hangi indekste hangi değerin olduğunu gösteren döngü
+setlocale(LC_ALL, "Turkish");
+int sayi[10];
+for (int i=0; i < 10; i++)//0 1 2.....9
+     sayi[i] = i+10;// sayi[0] , sayi[1]... sayi[9]
+//  10 11 12......19
+for (int i=0; i<10; i++)
+	printf("sayi[%d]. indeks elemanının değeri = %d \n", i, sayi[i]);
+
+getch();
+}
+
