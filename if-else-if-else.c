3d88464bad8b63d1e8a715fc47a6322265ecf2d0
jeenklynn <46195953+jeenklynn@users.noreply.github.com>
2022-04-24 14:04:20 +0300

    Create if-else-if-else.c

---
+++ if-else-if-else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+// iki sayıyı karşılaştıran program.
+{   
+	setlocale(LC_ALL, "Turkish");
+	
+	int sayi1,sayi2;
+	printf("Birinci Sayı: ");
+	scanf("%d",&sayi1);
+	printf("İkinci Sayı: ");
+	scanf("%d",&sayi2);
+	if(sayi1 > sayi2) // eğer birinci sayı ikinci sayıdan büyükse
+		printf("%d daha büyüktür.",sayi1);
+	// if altında tek satır kod var ise süslü parantez kullanmaya gerek yok
+	else if(sayi2>sayi1) // eğer ikinci sayı birinci sayıdan büyükse
+		printf("%d daha büyüktür.",sayi2);
+	else // eğer önceki kontrolleri sağlamadı ise
+		printf("Girilen sayılar birbirine eşittir");
+	getch();
+}
