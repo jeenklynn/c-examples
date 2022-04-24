a6624832f9944ea3decd02aa1f6456587f521831
jeenklynn <46195953+jeenklynn@users.noreply.github.com>
2022-04-24 14:12:03 +0300

    Update array.c

--- array.c
+++ array.c
@@ -10,7 +10,7 @@ int sayi[10];
 for (int i=0; i < 10; i++)//0 1 2.....9
      sayi[i] = i+10;// sayi[0] , sayi[1]... sayi[9]
 //  10 11 12......19
-for (int i=0; i<10; i++)
+for (int i = 0; i < 10; i++)
 	printf("sayi[%d]. indeks elemanının değeri = %d \n", i, sayi[i]);
 
 getch();
